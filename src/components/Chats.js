91fa836125937ad6a6c4825a8670267c669bfb32
vivek286 <solankivivek01@gmail.com>
2021-08-17 17:02:52 +0530

    fixing auth and chats

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,7 +1,33 @@
 import React from "react";
+import {useHistory} from 'react-router-dom'
+import {auth} from '../firebase'
+import {ChatEngine} from 'react-chat-engine'
 const Chats=()=>{
+    const history=useHistory();
+
+    const handlelogout= async()=>{
+        await auth.signOut();
+        history.push('/');
+    }
     return(
-        <h1>Chats</h1>
+    <div className="chats-page">
+        <div className="nav-bar">
+            <div className="logo-tab">
+                Chat App
+            </div>
+            <div className="logout-tab"
+            onClick={handlelogout}
+            >
+                Logout
+            </div>
+        </div>
+        <ChatEngine
+        height="calc(100vh-66px)"
+        projectId="143240cb-7fb7-40ef-8d7f-3a3b2a357ba7"
+        userName="."
+        userSecret="."
+        ></ChatEngine>
+    </div>
     )
 }
 
